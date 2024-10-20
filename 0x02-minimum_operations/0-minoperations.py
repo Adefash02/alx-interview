620b6608a4ea80f0e55538c02da0919fca27848f
Adefash02 <adelekeabdulmalikae@gmail.com>
2024-10-20 23:30:41 +0000

    debug

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,15 @@
 def minOperations(n):
     if n <= 1:
         return 0
-    
+
     operations = 0
-    factor = 2
+    divisor = 2
 
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
     return operations
 
